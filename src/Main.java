23a20f246138898d6a5cb0bdb824a843f31913be
Andreas Neik <andreas_neik@icloud.com>
2026-05-11 17:57:28 +0200

    Kommentare ergänzt Codestruktur verbessert

--- src/Main.java
+++ src/Main.java
@@ -12,25 +12,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Persistenzmanager erstellen.
+        PersistenceManager persistence = new PersistenceManager();
+
+        // Gespeicherte Daten laden.
+        TodoApp app = persistence.load();
+
+        // Controller erstellen, der Model und Persistenz verbindet.
+        TodoController controller = new TodoController(app, persistence);
+
+        // Beim Beenden automatisch den letzten Stand speichern.
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+
+            // Aktuellen Zustand der App dauerhaft sichern.
+            persistence.save(app);
+        }));
+
         // Startet die Swing-Oberfläche auf dem dafür vorgesehenen UI-Thread.
         SwingUtilities.invokeLater(() -> {
 
-            // Persistenzmanager erstellen.
-            PersistenceManager persistence = new PersistenceManager();
-
-            // Gespeicherte Daten laden.
-            TodoApp app = persistence.load();
-
-            // Controller erstellen, der Model und Persistenz verbindet.
-            TodoController controller = new TodoController(app, persistence);
-
             // GUI starten.
             new GUI(controller);
-
-            // Beim Beenden automatisch den letzten Stand speichern.
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                persistence.save(app);
-            }));
         });
     }
+
 }
